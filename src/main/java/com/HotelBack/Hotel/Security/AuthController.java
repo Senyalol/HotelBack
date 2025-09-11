b5dae2272614f3712477790720998391fbd03201
Simon <s.a.naumenko@students.psu.by>
2025-09-11 23:38:50 +0300

    add CORS configuration V1

--- src/main/java/com/HotelBack/Hotel/Security/AuthController.java
+++ src/main/java/com/HotelBack/Hotel/Security/AuthController.java
@@ -4,11 +4,9 @@ import com.HotelBack.Hotel.Security.SDTO.*;
 import com.HotelBack.Hotel.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = {"http://localhost:3000","http://localhost:5174"})
 @RequestMapping("/auth")
 @RestController
 public class AuthController {
